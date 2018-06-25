fbc6cc6470ae7a6a54c51d445615541b9c66ea01
Martin Kovac <martin_kovac@outlook.com>
2018-06-25 21:33:49 +0100

    Dummy navbar links added

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const NavBar = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  > * {
+    list-style: none;
+    text-decoration: none;
+  }
+`;
 const header = () => (
   <header>
-    <h1> Header to create </h1>
+    <nav>
+      <NavBar>
+        <li>Thumbtack Navbar to create =>>></li>
+        <Link to="/">Thumbtack</Link>
+        <Link to="/explore">Explore</Link>
+      </NavBar>
+    </nav>
   </header>
 );
 
