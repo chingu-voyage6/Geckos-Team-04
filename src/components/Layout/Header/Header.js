9ee91dcf7bd641aac61764b6623b5946f35d6a1e
Martin Kovac <martin_kovac@outlook.com>
2018-06-29 13:14:58 +0100

    Add header - desktop

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,24 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import logoFull from '../../../Assets/thumbtack.svg';
 
-const NavBar = styled.ul`
+const Navigation = styled.nav`
+  border-bottom: 1px solid #eee;
+  min-height: 30px;
   display: flex;
-  justify-content: space-around;
-  > * {
-    list-style: none;
-    text-decoration: none;
+  justify-content: space-between;
+
+  @media (min-width: 700px) {
+    border-bottom: 1px solid #eee;
+    min-height: 30px;
+    display: flex;
+    justify-content: space-between;
+  }
+  > ul {
+    display: flex;
+    align-items: center;
+    min-height: 100%;
+    margin: 0;
+    box-sizing: border-box;
+    > li {
+      list-style: none;
+      margin: 0 12px;
+      min-height: 100%;
+      display: flex;
+      align-items: center;
+      border-bottom: 2px solid rgba(80, 80, 80, 0);
+      > a {
+        text-decoration: none;
+        color: #676d73;
+      }
+      > a:hover {
+        color: #282828;
+      }
+    }
+    > li:hover {
+      border-bottom: 2px solid #808080;
+    }
   }
 `;
+
+const Logo = styled.img`
+  padding: 20px;
+  display: none;
+
+  @media (min-width: 700px) {
+    display: initial;
+  }
+`;
+
 const header = () => (
   <header>
-    <nav>
-      <NavBar>
-        <li>Thumbtack Navbar to create =>>></li>
-        <Link to="/">Thumbtack</Link>
-        <Link to="/explore">Explore</Link>
-      </NavBar>
-    </nav>
+    <div />
+    <Navigation>
+      <Link to="/">
+        <Logo src={logoFull} alt="logo" />
+      </Link>
+
+      <ul>
+        <li>
+          <Link to="/explore"> Explore </Link>
+        </li>
+        <li>
+          <Link to="/pro">Join as a Pro</Link>
+        </li>
+        <li>
+          <Link to="/choose-acount"> Sign Up </Link>
+        </li>
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+      </ul>
+    </Navigation>
   </header>
 );
 
