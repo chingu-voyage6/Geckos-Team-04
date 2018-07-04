430059e98297a6e097adef69fbb7abaeb060c973
Remy <jeremykent2726@gmail.com>
2018-07-04 12:52:26 +0300

    Modified landing page to easily take child elements

--- src/components/Pages/Landing.js
+++ src/components/Pages/Landing.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-import ServiceCard from '../Layout/Cards/ServiceCard/ServiceCard';
-import UserCard from '../Layout/Cards/UserCard/UserCard';
-import UtilCard from '../Layout/Cards/UtilCard/UtilCard';
+import PropTypes from 'prop-types';
 
 class Landing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      service: {
-        name: 'House Cleaning',
-        title: 'General House Cleaning',
-      },
-      utility: {
-        name: 'Events',
-        image: '',
-      },
-    };
-  }
-
   render() {
-    const { service, utility } = this.state;
-    return (
-      <div>
-        <UserCard />
-        <ServiceCard service={service} />
-        <UtilCard utility={utility} />
-      </div>
-    );
+    const { children } = this.props;
+    return <div>{children}</div>;
   }
 }
+
+Landing.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
 
 export default Landing;
