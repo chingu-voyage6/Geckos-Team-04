8bf622c0de7d6ec16bde526ebdf4e2af2d8620aa
Remy <jeremykent2726@gmail.com>
2018-07-04 07:10:09 +0300

    Added initial implementation of UtilCard component

---
+++ src/components/Layout/Cards/UtilCard/UtilCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Card from '../Card';
+import CardName from '../CardName';
+import CardBody from '../CardBody';
+import Icon from '../../Icon/Icon';
+
+const CardIconDiv = styled.div`
+  color: #a97ff0;
+`;
+
+const CardIcon = props => (
+  <Icon title="Icon name" {...props}>
+    <path d="" />
+  </Icon>
+);
+
+const UtilsCard = props => {
+  const {
+    utility: { name, image },
+  } = props;
+
+  return (
+    <Card>
+      <CardBody>
+        <CardIcon />
+        <CardName>{name}</CardName>
+      </CardBody>
+    </Card>
+  );
+};
+
+UtilsCard.propTypes = {
+  utility: PropTypes.object.isRequired,
+};
+
+export default UtilsCard;
