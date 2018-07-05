a8611e96dfe389b10ff606baf665d2a0c5cd577b
Martin Kovac <martin_kovac@outlook.com>
2018-07-05 16:02:16 +0100

    Page to include Layout with header and footer

--- src/components/Pages/Landing.js
+++ src/components/Pages/Landing.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Layout from '../Layout/Layout';
+import Section from '../Layout/Section/Section';
 
-const landing = () => <div>Wooow Landing</div>;
+const landing = () => (
+  <Layout>
+    <Section isGray hasBorder>
+      <div>Main</div>
+    </Section>
+  </Layout>
+);
 
 export default landing;
