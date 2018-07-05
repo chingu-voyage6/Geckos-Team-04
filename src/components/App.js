e963be11ac69fe87721b2c634d9539ffcf3c15fa
Martin Kovac <martin_kovac@outlook.com>
2018-07-05 16:00:06 +0100

    Route wrapper changed to Fragment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route } from 'react-router-dom';
 
-import Layout from './Layout/Layout';
-import Landing from './Pages/Landing';
-import Explore from './Pages/Explore';
+import LandingPage from './Pages/Landing';
+import ExplorePage from './Pages/Explore';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/explore" component={Explore} />
-      </Layout>
+      <Fragment>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/explore" component={ExplorePage} />
+      </Fragment>
     );
   }
 }
