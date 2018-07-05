197b27d15e8ab9d90df2dc78adce167b60e9da21
Martin Kovac <martin_kovac@outlook.com>
2018-07-05 16:47:06 +0100

    Add named landing page sections

--- src/components/Pages/Landing.js
+++ src/components/Pages/Landing.js
@@ -5,19 +5,22 @@ import Section from '../Layout/Section/Section';
 const landing = () => (
   <Layout>
     <Section isGray hasBorder>
-      <div>Main</div>
+      <h1>Hero</h1>
     </Section>
     <Section hasBorder>
-      <div>Main</div>
+      <h1>Popular services in your area</h1>
     </Section>
     <Section hasBorder>
-      <div>Main</div>
+      <h1>Large image carousel</h1>
     </Section>
     <Section isGray hasBorder>
-      <div>Main</div>
+      <h1>Home Weddings Events Wellness Lessons Pets(tabs thing with pictures)</h1>
     </Section>
     <Section hasBorder>
-      <div>Main</div>
+      <h1>Get free cost estimates.</h1>
+    </Section>
+    <Section isGray hasBorder>
+      <h1>Open for business.</h1>
     </Section>
   </Layout>
 );
