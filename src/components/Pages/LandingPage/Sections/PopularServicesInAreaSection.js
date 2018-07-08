1947080f00237061311a421a32a5c13dc4f38705
Martin Kovac <martin_kovac@outlook.com>
2018-07-08 20:20:29 +0100

    Updated to Carousel placeholder component

--- src/components/Pages/LandingPage/Sections/PopularServicesInAreaSection.js
+++ src/components/Pages/LandingPage/Sections/PopularServicesInAreaSection.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Section from '../../../Layout/Section/Section';
-import { ServiceLocationCard } from '../../../Shared/Cards/ServiceCard/ServiceCard';
+
+import { CarouselTempLocation } from '../../../Shared/CarouselTemp/CarouselTemp';
 
 const StyledSection = styled(Section)`
   > div {
@@ -9,28 +10,10 @@ const StyledSection = styled(Section)`
   }
 `;
 
-const CrouselNeeded = styled.div`
-  display: flex;
-`;
-
-const prop = {
-  service: {
-    name: 'House cleaning',
-    pros: Math.round(Math.random() * 1000),
-    image: 'https://source.unsplash.com/collection/1791908/250x350',
-  },
-  // image: 'http://thecatapi.com/api/images/get?format=src&size=full',
-  // image: 'http://thecatapi.com/api/images/get',
-  width: '33.3%',
-};
 const PopularServicesInAreaSection = () => (
   <StyledSection isGray hasBorder>
     <h3>Popular services in Your Area.</h3>
-    <CrouselNeeded>
-      <ServiceLocationCard {...prop} />
-      <ServiceLocationCard {...prop} />
-      <ServiceLocationCard {...prop} />
-    </CrouselNeeded>
+    <CarouselTempLocation />
   </StyledSection>
 );
 
