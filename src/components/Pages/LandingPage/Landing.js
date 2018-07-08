cf01657bb89b57b176e7e0544c243e0c44ae2766
Martin Kovac <martin_kovac@outlook.com>
2018-07-08 20:18:34 +0100

    Add sections - in progress

--- src/components/Pages/LandingPage/Landing.js
+++ src/components/Pages/LandingPage/Landing.js
@@ -5,6 +5,9 @@ import Section from '../../Layout/Section/Section';
 import HeroSection from './Sections/HeroSection';
 import NavPillBar from '../../Shared/NavPillBar/NavPillBar';
 import PopularServicesInAreaSection from './Sections/PopularServicesInAreaSection';
+import CategorieTabsSection from './Sections/CategoryTabs/CategoryTabsSection';
+import CostEstimateSection from './Sections/CostEstimateSection';
+import OpenForBusinessSection from './Sections/OpenForBusinessSection';
 
 const NavPillContainer = styled.div`
   height: 0;
@@ -20,15 +23,9 @@ const landing = () => (
     <Section hasBorder>
       <h1>Large image carousel</h1>
     </Section>
-    <Section isGray hasBorder>
-      <h1>Home Weddings Events Wellness Lessons Pets(tabs thing with pictures)</h1>
-    </Section>
-    <Section hasBorder>
-      <h1>Get free cost estimates.</h1>
-    </Section>
-    <Section isGray hasBorder>
-      <h1>Open for business.</h1>
-    </Section>
+    <CategorieTabsSection />
+    <CostEstimateSection />
+    <OpenForBusinessSection />
   </Layout>
 );
 
