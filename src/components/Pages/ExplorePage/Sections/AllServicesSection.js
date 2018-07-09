97c6be32287e4189cd1e2c08b359e72c0ad69dd6
Martin Kovac <martin_kovac@outlook.com>
2018-07-09 20:12:15 +0100

    Add AllSErivcesSection and CarouselSection

---
+++ src/components/Pages/ExplorePage/Sections/AllServicesSection.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Section from '../../../Layout/Section/Section';
+import { HomeCard } from '../../../Shared/Cards/UtilCard/UtilCard';
+
+const SectionTitle = styled.div`
+  text-align: left;
+`;
+const CardSubsection = styled.div`
+  div:first-child a {
+    border-radius: 4px 4px 0 0;
+  }
+
+  div:last-child a {
+    border-radius: 0 0 4px 4px;
+  }
+
+  display: block;
+  @media only screen and (min-width: 480px) {
+    div a {
+      border: initial;
+      border-radius: 4px;
+    }
+    div:first-child a {
+      border-radius: 4px;
+    }
+
+    div:last-child a {
+      border-radius: 4px;
+    }
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`;
+
+const allServicesSection = () => (
+  <Section isGray hasBorder>
+    <SectionTitle>
+      <h3>All Services</h3>
+    </SectionTitle>
+    <CardSubsection>
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+      <HomeCard utility={{ name: 'Home' }} />
+    </CardSubsection>
+  </Section>
+);
+
+export default allServicesSection;
