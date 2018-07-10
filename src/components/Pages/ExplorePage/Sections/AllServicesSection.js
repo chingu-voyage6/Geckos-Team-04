22075fa0dc587def82db65b641b22195ba811c22
Martin Kovac <martin_kovac@outlook.com>
2018-07-10 22:11:39 +0100

    Add content data for cards

--- src/components/Pages/ExplorePage/Sections/AllServicesSection.js
+++ src/components/Pages/ExplorePage/Sections/AllServicesSection.js
@@ -4,6 +4,68 @@ import PropTypes from 'prop-types';
 import Section from '../../../Layout/Section/Section';
 import { HomeCard } from '../../../Shared/Cards/UtilCard/UtilCard';
 
+const cardData = [
+  {
+    component: HomeIcon,
+    to: 'home-improvement',
+    name: 'Home',
+  },
+  {
+    component: EventIcon,
+    to: 'events',
+    name: 'Events',
+  },
+  {
+    component: AppleIcon,
+    to: 'lessons',
+    name: 'Lessons',
+  },
+  {
+    component: FlowerIcon,
+    to: 'wellness',
+    name: 'Wellness',
+  },
+  {
+    component: BriefCaseIcon,
+    to: 'more-services#category-group-business',
+    name: 'Business',
+  },
+  {
+    component: CraftsIcon,
+    to: 'more-services#category-group-crafts',
+    name: 'Crafts',
+  },
+  {
+    component: WebIcon,
+    to: 'more-services#category-group-designWeb',
+    name: 'Design & Web',
+  },
+  {
+    component: LegalIcon,
+    to: 'more-services#category-group-legal',
+    name: 'Legal',
+  },
+  {
+    component: HandIcon,
+    to: 'more-services#category-group-personal',
+    name: 'Personal',
+  },
+  {
+    component: PawIcon,
+    to: 'more-services#category-group-pets',
+    name: 'Pets',
+  },
+  {
+    component: CameraIcon,
+    to: 'more-services#category-group-photography',
+    name: 'Photography',
+  },
+  {
+    component: ServiceIcon,
+    to: 'more-services#category-group-repairSupport',
+    name: 'Tech Repair',
+  },
+];
 const SectionTitle = styled.div`
   text-align: left;
 `;
