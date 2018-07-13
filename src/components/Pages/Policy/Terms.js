a38ff4fe1e942493c79c68aa8fc612eeae37043d
Remy <jeremykent2726@gmail.com>
2018-07-13 17:22:22 +0300

    Fixed some formatting issues

--- src/components/Pages/Policy/Terms.js
+++ src/components/Pages/Policy/Terms.js
@@ -92,6 +92,276 @@ const Policy = () => (
       "Pro Services" means the services listed, quoted, scheduled, offered or provided by Service
       Members, or sought, scheduled or received by Customer Members, through the Platform.
     </p>
+
+    <p>
+      "Service Member" means a Member who is registered to send quotes for Pro Services, sends
+      quotes for Pro Services, or otherwise uses the Platform to offer, provide, receive payment
+      for, or facilitate the provision of Pro Services.
+    </p>
+
+    <p>
+      "Thumbtack Content" means all Content Thumbtack makes available on or through the Platform,
+      including any Content licensed from a third party, but excluding User Content.
+    </p>
+
+    <p>
+      "User Content" means all Content submitted, posted, uploaded, published, or transmitted on or
+      through the Platform by any Member or other user of the Platform, including but not limited to
+      photographs, profile information, descriptions, postings, reviews, and payments made through
+      the Platform, but excluding Thumbtack Content and
+      <a href="#feedback">Feedback</a>.
+    </p>
+
+    <h3 id="ELIGIBILITY-INFO">
+      ELIGIBILITY, SERVICE MEMBER REPRESENTATIONS, WARRANTIES, AND USE OF THE PLATFORM
+    </h3>
+
+    <p>
+      Access to and use of the Platform is available only to individuals who are at least 18 years
+      old and can form legally binding contracts under applicable law. By accessing or using the
+      Platform, you represent and warrant that you are eligible.
+    </p>
+
+    <p>
+      By registering or using the Platform to offer, post or provide Pro Services, Service Members
+      represent and warrant that they, and the employees, agents, contractors, and subcontractors
+      who may perform work for them, are properly and fully qualified and experienced, and licensed,
+      certified, bonded, and insured, as required by applicable laws or regulations to which they
+      may be subject in the jurisdiction(s) in which they offer their Pro Services and in relation
+      to the specific job they are performing.{' '}
+    </p>
+
+    <p>
+      Thumbtack is not in the business of providing Pro Services. Service Members understand and
+      agree that by creating and maintaining an account on the Platform, they receive only the
+      ability to use the Thumbtack Platform to access persons interested in receiving Pro Services
+      and related tools, including but not limited to the ability to message Customer Members or
+      schedule appointments, that facilitate the provision of Pro Services. Service Members
+      understand and agree that using the Platform does not guarantee that any Thumbtack users will
+      engage them for Pro Services.
+    </p>
+
+    <p>
+      Service Members understand and agree that they are customers of Thumbtack, and are not
+      Thumbtack employees, joint venturers, partners, or agents. Service Members acknowledge that
+      they set or confirm their own prices, provide their own equipment, and determine their own
+      work schedule. Thumbtack does not control, and has no right to control, the services a Service
+      Member provides (including how the Service Member provides such services) if the Service
+      Member is engaged by a Customer Member, except as specifically noted herein.
+    </p>
+
+    <p>
+      Thumbtack, as permitted by applicable laws, obtains reports based on publicly available
+      information regarding Service Members, which may include history of criminal convictions or
+      sex offender registration, and we may limit, block, suspend, deactivate, or cancel a Service
+      Member’s account based on the results of such a check. As a Service Member, you agree and
+      authorize us to use your personal information, such as your full name and date of birth, to
+      obtain such reports from Thumbtack’s vendors.
+    </p>
+
+    <h3>ACCOUNT REGISTRATION AND OTHER SUBMISSIONS</h3>
+
+    <p>
+      Users may access the Platform without registering for an account. To access and participate in
+      certain features of the Platform, you will need to create a password-protected account
+      ("Account"). You may register for an Account using your existing Facebook account and log-in
+      credentials (your "Third-Party Site Password"). You agree to provide accurate, current, and
+      complete information during the registration or request submission process and at all other
+      times when you use the Platform, and to update information to keep it accurate, current, and
+      complete. You are solely responsible for safeguarding your Thumbtack password and, if
+      applicable, your Third-Party Site Password. You are solely responsible for all activity that
+      occurs on your Account, and you will notify Thumbtack immediately of any unauthorized use.
+      Thumbtack is not liable for any losses by any party caused by an unauthorized use of your
+      Account. Notwithstanding the foregoing, you may be liable for the losses of Thumbtack or
+      others due to such unauthorized use.
+    </p>
+
+    <h3>YOUR LICENSE TO USE THE PLATFORM</h3>
+
+    <p>
+      Subject to your compliance with these Terms, Thumbtack grants you a limited, non-exclusive,
+      revocable, nontransferable, and non-sublicensable license to reproduce and display Collective
+      Content (excluding any software source code) solely for your personal and non-commercial use
+      and only in connection with your access to and participation in the Platform. You will not
+      use, copy, adapt, modify, prepare derivative works based upon, distribute, license, sell,
+      transfer, publicly display, publicly perform, transmit, stream, broadcast or otherwise exploit
+      the Platform or Collective Content, except as expressly permitted in these Terms. The Platform
+      and Collective Content are provided to you AS IS. If you download or print a copy of
+      Collective Content for personal use, you must retain all copyright and other proprietary
+      notices contained thereon. No licenses or rights are granted to you by implication or
+      otherwise under any intellectual property rights owned or controlled by Thumbtack or its
+      licensors, except for the licenses and rights expressly granted in these Terms.
+    </p>
+
+    <h3>USER CONTENT</h3>
+
+    <p>
+      We may, in our sole discretion, permit you to post, upload, publish, submit or transmit User
+      Content. By making available any User Content on or through the Platform, you hereby grant to
+      Thumbtack a worldwide, irrevocable, perpetual, non-exclusive, transferable, royalty-free
+      license, with the right to sublicense, to use, copy, adapt, modify, distribute, license, sell,
+      transfer, publicly display, publicly perform, transmit, stream, broadcast, access, view, and
+      otherwise exploit such User Content on, through, by means of or to promote, market or
+      advertise the Platform or Pro Services, or for any other purpose in our sole discretion,
+      except that private messaging through the Platform will not be used by Thumbtack in public
+      advertising. In the interest of clarity, the license granted to Thumbtack shall survive
+      termination of the Platform or your Account. Thumbtack does not claim ownership rights in your
+      User Content and nothing in these Terms will be deemed to restrict rights that you may have to
+      use and exploit any such User Content submitted, posted, uploaded, published, or transmitted
+      on or through the Platform by you.
+    </p>
+
+    <p>
+      You acknowledge and agree that you are solely responsible for all User Content that you make
+      available on or through the Platform. Accordingly, you represent and warrant that: (a) you
+      either are the sole and exclusive owner of all User Content that you make available on or
+      through the Platform or you have all rights, licenses, consents and releases that are
+      necessary to grant to Thumbtack the rights in such User Content, as contemplated under these
+      Terms; and (b) neither the User Content nor your posting, uploading, publication, submission
+      or transmittal of the User Content or Thumbtack's use of your User Content (or any portion
+      thereof) on, through or by means of the Platform will infringe, misappropriate or violate a
+      third party's patent, copyright, trademark, trade secret, moral rights or other proprietary or
+      intellectual property rights, or rights of publicity or privacy, or result in the violation of
+      any applicable law or regulation. You agree that Thumbtack may proofread, summarize or
+      otherwise edit and/or withdraw your User Content,and you understand it remains your sole
+      responsibility to monitor your User Content and ensure that such edited Content is accurate
+      and consistent with your representations and warranties in these Terms.
+    </p>
+
+    <p>
+      You acknowledge and agree that you are solely responsible for all User Content that you make
+      available on or through the Platform. Accordingly, you represent and warrant that: (a) you
+      either are the sole and exclusive owner of all User Content that you make available on or
+      through the Platform or you have all rights, licenses, consents and releases that are
+      necessary to grant to Thumbtack the rights in such User Content, as contemplated under these
+      Terms; and (b) neither the User Content nor your posting, uploading, publication, submission
+      or transmittal of the User Content or Thumbtack's use of your User Content (or any portion
+      thereof) on, through or by means of the Platform will infringe, misappropriate or violate a
+      third party's patent, copyright, trademark, trade secret, moral rights or other proprietary or
+      intellectual property rights, or rights of publicity or privacy, or result in the violation of
+      any applicable law or regulation. You agree that Thumbtack may proofread, summarize or
+      otherwise edit and/or withdraw your User Content,and you understand it remains your sole
+      responsibility to monitor your User Content and ensure that such edited Content is accurate
+      and consistent with your representations and warranties in these Terms.
+    </p>
+
+    <p>
+      Thumbtack reserves the right, at any time and without prior notice, to remove or disable
+      access to User Content that we, in our sole discretion, consider to be objectionable for any
+      reason, in violation of these Terms or otherwise harmful to the Platform or users, or for any
+      other reason.
+    </p>
+
+    <h3>PROHIBITIONS</h3>
+
+    <p>As a user of the Platform, you may not:</p>
+    <ul>
+      <li>
+        Use another person's Account, misrepresent yourself or Pro Services offered through the
+        Platform, misrepresent your identity or qualifications, misrepresent a project or other
+        information in a quote request, or post Content in any inappropriate category or areas on
+        the Platform;
+      </li>
+      <li>
+        Use any automated system including but not limited to robots, spiders, offline readers,
+        scrapers to access the Platform for any purpose without Thumbtack's prior written
+        approval;provided, however, that the operators of public search engines may use spiders or
+        robots to copy materials from the Platform for the sole purpose of creating publicly
+        available searchable indices of the materials, but not caches or archives of such material
+        (Thumbtack reserves the right to revoke these exceptions either generally or in specific
+        cases);
+      </li>
+      <li>
+        In any manual or automated manner copy copyrighted text, or otherwise misuse or
+        misappropriate Platform information or Content including but not limited to, use on a
+        mirrored, competitive, or third-party site;
+      </li>
+      <li>
+        Transmit more request messages through the Platform in a given period of time than a human
+        can reasonably produce in the same period by using a conventional online web browser;
+      </li>
+      <li>
+        Take any action that (a) may unreasonably encumber the Platform's infrastructure; (b)
+        interferes or attempts to interfere with the proper working of the Platform or any
+        third-party participation; (c) bypasses measures that are used to prevent or restrict access
+        to the Platform; (d) circumvents, disables or otherwise interferes with security features of
+        the Platform; (e) distributes viruses or any other technologies that may harm Thumbtack or
+        users; (f) uses the Platform in a way that violates any copyrights, trade secrets, or other
+        rights of any third party, including privacy or publicity rights; or (g) circumvents or
+        manipulates Fee (defined herein) structure, billing, or Fees owed;
+      </li>
+      <li>
+        As a Service Member, use the Platform in any manner that circumvents your obligation to pay
+        Thumbtack or use credits for access to services provided by the Platform;
+      </li>
+      <li>
+        Collect, harvest or publish any personally identifiable data including but not limited to
+        names or other account information, from the Platform, or use the communication systems
+        provided by the Platform for any reason not explicitly authorized by these Terms, including
+        commercial solicitation purposes;
+      </li>
+      <li>
+        Recruit, solicit, or contact in any form Service Members or Customer Members for employment
+        or any other use not specifically intended by the Platform;
+      </li>
+      <li>
+        Take any inappropriate or unlawful actions, including the submission of inappropriate or
+        unlawful Content to or through the Platform, including Content that is harassing, hateful,
+        illegal, profane, obscene, defamatory, threatening, or discriminatory, or that advocates,
+        promotes, or encourages inappropriate activity, conduct that would be considered a criminal
+        offense, or conduct that would give rise to civil liability or violate any law;
+      </li>
+      <li>
+        Violate any key{' '}
+        <a href="https://help.thumbtack.com/?ar=kA131000000NylD">Thumbtack policies</a> that govern
+        your use of the Platform and our interactions with you and third parties;
+      </li>
+      <li>
+        Advertise or solicit a Pro Service not related to or appropriate for the Platform including,
+        but not limited to any Pro Service that (a) is not in supported categories or provides only
+        products; (b) provides directories or referrals; (c) offers lending; (d) offers rental
+        space; (e) promotes events (such as a party or professional convention); (f) competes with
+        the business of Thumbtack; (g) is based outside the United States; (h) promotes or offers
+        Ponzi schemes, junk mail, spam, chain letters, pyramid schemes, affiliate marketing or
+        unsolicited commercial content, discount cards, credit counseling, online surveys or
+        contests, raffles, prizes, bonuses, games of chance or giveaways; or (i) provides non-local
+        content.
+      </li>
+      <li>
+        Submit User Content that damages the experience of any user including but not limited to (a)
+        requests to download non-Thumbtack mobile applications and/or links that direct the user to
+        mirrored websites where the user must enter information that is redundant with what has
+        already been entered on Thumbtack, (b) offers to purchase a Pro Service or any other service
+        outside of Thumbtack, or (c) using a profile page or user name to promote services not
+        offered on or through the Platform;
+      </li>
+      <li>
+        Take any action that may undermine the efficacy or accuracy of reviews or ratings systems;
+      </li>
+      <li>
+        Fail to perform Pro Services purchased from you as promised, unless the Customer Member
+        fails to materially meet the terms of the mutually agreed-upon agreement for the Pro
+        Services or refuses to pay, or a clear typographical error is made, or you cannot
+        authenticate the Customer Member's identity;
+      </li>
+
+      <li>
+        Engage in fraudulent conduct including but not limited to offering to make money transfers
+        with intent to request a refund of any portion of the payment or soliciting users to mail
+        cash or use other payment methods prohibited by Thumbtack;
+      </li>
+
+      <li>
+        Sign up for, negotiate a price for, use, or otherwise solicit a Pro Service with no
+        intention of following through with your use of or payment for the Pro Service;
+      </li>
+      <li>Agree to purchase a Pro Service when you do not meet a Service Member's requirements;</li>
+      <li>
+        Undertake any activity or engage in any conduct that is inconsistent with the business or
+        purpose of the Platform; and
+      </li>
+      <li>Attempt to indirectly undertake any of the foregoing.</li>
+    </ul>
   </PolicySnap>
 );
 
