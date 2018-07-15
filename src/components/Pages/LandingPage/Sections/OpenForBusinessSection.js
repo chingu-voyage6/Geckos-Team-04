afb2c15ba9cd9f3b2321d35fca7af397d6157905
Martin Kovac <martin_kovac@outlook.com>
2018-07-15 09:56:32 +0100

    Add responsivness to OpenForBusinessSection

--- src/components/Pages/LandingPage/Sections/OpenForBusinessSection.js
+++ src/components/Pages/LandingPage/Sections/OpenForBusinessSection.js
@@ -9,29 +9,72 @@ import Section from '../../../Layout/Section/Section';
 const StyledSection = styled(Section)`
   > div {
     display: flex;
+    flex-direction: column;
+
+    @media (min-width: 482px) {
+      display: flex;
+      flex-direction: row;
+    }
   }
 `;
 
 const SectionHalf = styled.div`
-  max-width: 50%;
+  max-width: 100%;
   display: block;
   min-height: 100%;
-  display: flex;
   align-items: center;
+  position: relative;
+  @media (min-width: 482px) {
+    max-width: 50%;
+    padding: 0;
+    display: flex;
+  }
 `;
+
+const StyledSectionHalf = SectionHalf.extend``;
 
 const StyledImage = styled.img`
   transform: scale(1.2);
+  left: 50px;
+  top: 10px;
+  display: block;
+  position: relative;
+
+  @media (min-width: 482px) {
+    left: 0;
+  }
+  @media (min-width: 701px) {
+    transform: scale(1.2);
+    display: initial;
+    position: initial;
+  }
 `;
 
 const SectionText = styled.div`
   color: #676d73;
   font-size: 0.9rem;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
+
+  @media (min-width: 701px) {
+    margin-bottom: 2rem;
+  }
 `;
 
 const SectionTitle = styled.div`
-  margin-bottom: 1rem;
+  white-space: nowrap;
+  margin-bottom: 0.5rem;
+  @media (min-width: 701px) {
+    margin-bottom: 1rem;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  margin: 50px 0;
+  padding-left: 0;
+  @media (min-width: 701px) {
+    margin: initial;
+    padding-left: 1rem;
+  }
 `;
 
 const openForBusinessSection = () => (
@@ -43,7 +86,7 @@ const openForBusinessSection = () => (
       />
     </SectionHalf>
     <SectionHalf>
-      <div>
+      <ContentWrapper>
         <SectionTitle>
           <h2>Open for business.</h2>
         </SectionTitle>
@@ -51,7 +94,7 @@ const openForBusinessSection = () => (
           <p>Whatever work you do, we'll find you the jobs you want.</p>
         </SectionText>
         <DefaultButton>Become a Thumbtack pro</DefaultButton>
-      </div>
+      </ContentWrapper>
     </SectionHalf>
   </StyledSection>
 );
