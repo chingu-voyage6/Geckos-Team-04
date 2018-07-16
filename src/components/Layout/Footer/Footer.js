9b70dc9cc91fa64b0d5934ca4178a3c4bf1734f7
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-07-16 03:41:01 +0100

    mobile footer reverted back to solve breaking desktop footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import MobileFooterInner from './MobileFooterInner';
 
 const FooterSection = styled.footer`
   padding: 48px 0 0 0;
   margin: 10px 0 0 0;
   width: 100%;
 `;
-
 const StyledFooter = styled.div`
   display: flex;
   width: 100%;
@@ -20,33 +20,7 @@ const StyledFooter = styled.div`
     display: none;
   }
 `;
-const MobileFooter = styled.div`
-  width: 100%;
-  height: auto;
-  padding: 0 20px;
-  display: none;
-  @media (max-width: 700px) {
-    display: initial;
-  }
-`;
-const MobileLinkContainer = styled.div`
-  border-bottom: 1px solid #dae1e7;
-  display: flex;
-`;
-const MobileLinkElement1 = styled.div`
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  font-size: 2rem;
-  width: 90%;
-  flex-grow: 1;
-`;
-const MobileLinkElement2 = styled.div`
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  width: 10%;
-  flex-grow: 1;
-  text-align: right;
-`;
+
 const FooterCol1 = styled.div`
   width: 33.33333%;
 `;
@@ -112,56 +86,7 @@ const Footer = props => {
 
   return (
     <FooterSection>
-      <MobileFooter>
-        <MobileLinkContainer>
-          <MobileLinkElement1>Company</MobileLinkElement1>
-          <MobileLinkElement2>
-            <svg width="16" height="9" viewBox="0 0 16 9">
-              <path
-                d="M8.008 9a.497.497 0 0 1-.36-.157L.138.862a.515.515 0 0 1 .015-.72.493.493 0 0 1 .707.015l7.147 7.596 7.13-7.596a.494.494 0 0 1 .707-.016c.2.196.207.518.016.721L8.37 8.843A.495.495 0 0 1 8.01 9z"
-                fill="#979797"
-                fillRule="evenodd"
-              />
-            </svg>
-          </MobileLinkElement2>
-        </MobileLinkContainer>
-        <MobileLinkContainer>
-          <MobileLinkElement1>Customers</MobileLinkElement1>
-          <MobileLinkElement2>
-            <svg width="16" height="9" viewBox="0 0 16 9">
-              <path
-                d="M8.008 9a.497.497 0 0 1-.36-.157L.138.862a.515.515 0 0 1 .015-.72.493.493 0 0 1 .707.015l7.147 7.596 7.13-7.596a.494.494 0 0 1 .707-.016c.2.196.207.518.016.721L8.37 8.843A.495.495 0 0 1 8.01 9z"
-                fill="#979797"
-                fillRule="evenodd"
-              />
-            </svg>
-          </MobileLinkElement2>
-        </MobileLinkContainer>
-        <MobileLinkContainer>
-          <MobileLinkElement1>Pros</MobileLinkElement1>
-          <MobileLinkElement2>
-            <svg width="16" height="9" viewBox="0 0 16 9">
-              <path
-                d="M8.008 9a.497.497 0 0 1-.36-.157L.138.862a.515.515 0 0 1 .015-.72.493.493 0 0 1 .707.015l7.147 7.596 7.13-7.596a.494.494 0 0 1 .707-.016c.2.196.207.518.016.721L8.37 8.843A.495.495 0 0 1 8.01 9z"
-                fill="#979797"
-                fillRule="evenodd"
-              />
-            </svg>
-          </MobileLinkElement2>
-        </MobileLinkContainer>
-        <MobileLinkContainer>
-          <MobileLinkElement1>Support</MobileLinkElement1>
-          <MobileLinkElement2>
-            <svg width="16" height="9" viewBox="0 0 16 9">
-              <path
-                d="M8.008 9a.497.497 0 0 1-.36-.157L.138.862a.515.515 0 0 1 .015-.72.493.493 0 0 1 .707.015l7.147 7.596 7.13-7.596a.494.494 0 0 1 .707-.016c.2.196.207.518.016.721L8.37 8.843A.495.495 0 0 1 8.01 9z"
-                fill="#979797"
-                fillRule="evenodd"
-              />
-            </svg>
-          </MobileLinkElement2>
-        </MobileLinkContainer>
-      </MobileFooter>
+      <MobileFooterInner />
       <StyledFooter>
         <FooterCol1>
           <svg width="120" height="17" viewBox="0 0 156 21" fill="currentColor">
