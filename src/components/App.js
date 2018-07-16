b4afc8bdca7378dbc5f146cc73e1189e334fc1a0
Vlad Bereznyuk <vbereznuk@gmail.com>
2018-07-16 22:46:28 +0500

    feature/acount-settings route to new page

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import TermsPage from './Pages/Terms';
 import LoginPage from './Pages/Login';
 import MoreServicesPage from './Pages/MoreServicesPage/MoreServices';
 import CustomerProfile from './Pages/Customer/Profile';
+import AccountSettings from './Pages/Customer/Information';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
         <Route exact path="/login" component={LoginPage} />
         <Route path="/more-services" component={MoreServicesPage} />
         <Route exact path="/customer/profile" component={CustomerProfile} />
+        <Route exact path="/profile/information" component={AccountSettings} />
       </Fragment>
     );
   }
