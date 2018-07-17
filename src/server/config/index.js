2fab1e34db97629d48d5b2f33fc2163cb730f5bb
Remy <jeremykent2726@gmail.com>
2018-07-17 12:37:45 +0300

    Added a way to secure backend routes

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -4,4 +4,5 @@ module.exports = {
     development: 'mongodb://localhost:27017/thumbtack',
     test: 'mongodb://localhost:27017/thumbtack-test',
   },
+  secret: process.env.SECRET_KEY,
 };
--- src/server/index.js
+++ src/server/index.js
@@ -8,15 +8,13 @@ const app = express();
 
 const config = require('./config');
 
-// Sanity check
-app.get('/', (req, res) => console.log('Got a request!'));
-
+app.set('secret', config.secret);
 app.set('dbUrl', config.db[app.settings.env]);
 mongoose.connect(app.get('dbUrl'));
 
 app.use('/api/v1/', routes);
 
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
