011a5f495a02722bd2be1867299c876ed4ff9b6b
Martin Kovac <martin_kovac@outlook.com>
2018-07-23 17:57:02 +0100

    Add backdrop component

---
+++ src/components/Pages/ServiceNearMePage/DialogueModal/Backdrop.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackDrop = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 100000;
+  top: 0;
+`;
