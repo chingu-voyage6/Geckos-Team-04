fd9ad2bfef0dc4e8bc2c92ae80cfa04afe255dad
Martin Kovac <martin_kovac@outlook.com>
2018-07-23 17:58:11 +0100

    Add modal footer

---
+++ src/components/Pages/ServiceNearMePage/DialogueModal/ModalFooter.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { DefaultButton } from '../../../Shared/UI/Button/Button';
+
+const FooterBody = styled.div`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  background-color: ${({ isBlue }) => (isBlue ? '#009fd9' : '#fff')};
+  border-top: 1px solid ${({ isBlue }) => (isBlue ? '#009fd9' : '#e9eced')};
+`;
+
+const ButtonWhite = DefaultButton.extend`
+  background-color: #ffffff;
+  color: black;
+  min-width: 50%;
+
+  width: auto;
+  :focus {
+    background-color: #ffffff;
+    color: black;
+    text-decoration: underline;
+  }
+`;
+
+const ButtonGray = DefaultButton.extend`
+  border: 2px solid #d3d4d5;
+  color: #676d73;
+  background-color: #fff;
+  margin-right: 20px;
+  min-width: 25%;
+  width: auto;
+
+  :focus {
+    border-color: #2f3033;
+    background: #fff;
+  }
+`;
+
+const ButtonBlue = DefaultButton.extend`
+  min-width: 25%;
+  width: auto;
+`;
+
+const ModalFooter = ({ option, nextSlide, previousSlide, isBlue }) => (
+  <FooterBody isBlue={isBlue}>
+    {(() => {
+      switch (option) {
+        case 'backAndSkip':
+          return (
+            <Fragment>
+              <ButtonGray onClick={previousSlide}>Back</ButtonGray>
+              <ButtonBlue onClick={nextSlide}>Skip</ButtonBlue>
+            </Fragment>
+          );
+
+        case 'backAndNext':
+          return (
+            <Fragment>
+              <ButtonGray onClick={previousSlide}>Back</ButtonGray>
+              <ButtonBlue onClick={nextSlide}>Next</ButtonBlue>
+            </Fragment>
+          );
+        case 'next':
+          return <ButtonWhite onClick={nextSlide}>Next</ButtonWhite>;
+
+        default:
+          return null;
+      }
+    })()}
+  </FooterBody>
+);
+
+ModalFooter.propTypes = {
+  option: PropTypes.string.isRequired,
+  isBlue: PropTypes.bool.isRequired,
+  nextSlide: PropTypes.func.isRequired,
+  previousSlide: PropTypes.func.isRequired,
+};
+
+export default ModalFooter;
