a2cc6fecd75e83974d772fa6c0a0903c58b87917
Vlad Bereznyuk <vbereznuk@gmail.com>
2018-07-26 12:02:19 +0500

    feature/user-signup-and-authentication User model modification + minor addition to index.js server for new user and auth routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compress = require('compression');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -11,6 +16,18 @@ const config = require('./config');
 app.set('secret', config.secret);
 app.set('dbUrl', config.db[app.settings.env]);
 mongoose.connect(app.get('dbUrl'));
+
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/v1/', routes);
 
