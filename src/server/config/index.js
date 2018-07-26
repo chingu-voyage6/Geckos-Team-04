0336ec1ce124bcec7b27e5acca37a07c8d5bef07
Vlad Bereznyuk <vbereznuk@gmail.com>
2018-07-26 12:05:30 +0500

    mongodb dev location fix

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,8 +1,7 @@
 module.exports = {
   db: {
     production: process.env.DB_URL,
-    // development: 'mongodb://localhost:27017/thumbtack',
-    development: 'mongodb://192.168.99.100:27017/thumbtack',
+    development: 'mongodb://localhost:27017/thumbtack',
     test: 'mongodb://localhost:27017/thumbtack-test',
   },
   secret: process.env.SECRET_KEY || 'secret',
