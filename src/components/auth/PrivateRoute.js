61bae4c127f40c6743224f0f9e6dc44009efacde
Vlad Bereznyuk <vbereznuk@gmail.com>
2018-07-27 14:35:58 +0500

    feature/user-signup-and-authentication client api-auth api-user libs to talk with the backend & PrivateRoute wrapper

---
+++ src/components/auth/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './auth-helper';
+
+const PrivateRoute = ({ ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
