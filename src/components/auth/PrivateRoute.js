5789e6217dbc4530c664c90c6fb29137dfc1c1ce
Martin Kovac <martin_kovac@outlook.com>
2018-08-12 16:25:16 +0100

    Remove auto update import error

--- src/components/auth/PrivateRoute.js
+++ src/components/auth/PrivateRoute.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import {
-  Route,
-  Redirect,
-} from 'C:/Users/marti/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-router-dom';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { isAuthenticated } from './auth-helper';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
